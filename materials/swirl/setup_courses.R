6e45fa6026749cdf5bec297347b85dd438fa1b69
Andrey Koval <koval.andrey@gmail.com>
2015-09-11 11:47:24 -0700

    update

--- materials/swirl/setup_courses.R
+++ materials/swirl/setup_courses.R
@@ -1,8 +1,15 @@
 # https://github.com/swirldev/swirl_courses
 
-# install.packages("swirl")
+# if you haven't installed swirl before run
+# install.packages("swirl")  
+# install.packages("Rtools")  
+# install.packages("devtools")  
+# devtools::install_github(c("swirldev/swirl", "swirldev/swirlify"))  
 
+# load the library
 library(swirl)
+
+# install individual course described at https://github.com/swirldev/swirl_courses 
 install_from_swirl("Data_Analysis")
 install_from_swirl("Exploratory_Data_Analysis")
 install_from_swirl("Getting_and_Cleaning_Data")
@@ -13,6 +20,7 @@ install_from_swirl("Regression_Models")
 install_from_swirl("Statistical_Inference")
 install_from_swirl("Writing_swirl_Courses")
 
+# when you are ready to start swirling, type
 swirl()
 
 # | When you are at the R prompt with swirl (>):
